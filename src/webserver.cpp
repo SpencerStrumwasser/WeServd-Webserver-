d721193a6811935b605c474e72de6aca86f272bc
Spencer Strumwasser <sstrumwa@caltech.edu>
2015-04-22 16:02:30 -0700

    I have changed the file so it represents the echo serer, I have changed a few thing in addition, but am still working on it

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -1,19 +1,71 @@
+#include <cstdlib>
 #include <iostream>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
 
-#include "parser/config_parser.h"
+using boost::asio::ip::tcp;
 
-using namespace std;
+const int max_length = 1024;
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Usage: ./webserver <config file>\n");
-        return 1;
+typedef boost::shared_ptr<tcp::socket> socket_ptr;
+
+void session(socket_ptr sock)
+{
+    try
+    {
+        for (;;)
+        {
+            char data[max_length];
+
+            boost::system::error_code error;
+            size_t length = sock->read_some(boost::asio::buffer(data), error);
+            if (error == boost::asio::error::eof)
+                break; // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+
+            boost::asio::write(*sock, boost::asio::buffer(data, length));
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception in thread: " << e.what() << "\n";
+    }
+}
+
+void server(boost::asio::io_service& io_service, unsigned short port)
+{
+    tcp::acceptor a(io_service, tcp::endpoint(tcp::v4(), port));
+    for (;;)
+    {
+        socket_ptr sock(new tcp::socket(io_service));
+        a.accept(*sock);
+        boost::thread t(boost::bind(session, sock));
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2) {
+            printf("Usage: ./webserver <config file>\n");
+            return 1;
+        }
+        NginxConfigParser config_parser;
+        NginxConfig config;
+        config_parser.Parse(argv[1], &config);
+
+        boost::asio::io_service io_service;
+
+        using namespace std; // For atoi.
+        server(io_service, atoi(argv[1]));
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
-    NginxConfigParser config_parser;
-    NginxConfig config;
-    config_parser.Parse(argv[1], &config);
-
-    printf("%s", config.ToString().c_str());
     return 0;
-}

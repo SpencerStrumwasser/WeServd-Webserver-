78f2bfab79209737c3367ebf57a3509259dcf577
Spencer Strumwasser <sstrumwa@caltech.edu>
2015-05-01 12:49:04 -0700

    added some unit tests to check for port number when the file is not simple, one includes port and number on different lines, and one includes wher there are other things besides just port

--- tests/parser/ServerTest.cpp
+++ tests/parser/ServerTest.cpp
@@ -2,12 +2,37 @@
 
 #include "../../src/parser/ParserProcessor.h"
 
-
+//Test a simple situation
 TEST(SamePort, GetPort)
 {	
 	NginxConfigParser config_parser;
     NginxConfig config;
     config_parser.Parse("default_config", &config);
+    ParserProcessor parser_processor = ParserProcessor(config);
+    unsigned short port = parser_processor.get_port();
+    EXPECT_EQ(8080, port);
+
+}
+
+//Tests where the port and the number arent on the sae line
+TEST(SamePort, GetPort2)
+{	
+	NginxConfigParser config_parser;
+    NginxConfig config;
+    config_parser.Parse("default_test_config", &config);
+    ParserProcessor parser_processor = ParserProcessor(config);
+    unsigned short port = parser_processor.get_port();
+    EXPECT_EQ(8080, port);
+
+}
+
+
+//Tests where the port is not the only thing in the file
+TEST(SamePort, GetPort3)
+{	
+	NginxConfigParser config_parser;
+    NginxConfig config;
+    config_parser.Parse("test_config", &config);
     ParserProcessor parser_processor = ParserProcessor(config);
     unsigned short port = parser_processor.get_port();
     EXPECT_EQ(8080, port);

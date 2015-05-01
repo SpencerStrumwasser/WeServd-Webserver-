de46ca1090d137d70ab95892b078ab2d28685638
Spencer Strumwasser <sstrumwa@caltech.edu>
2015-05-01 04:07:32 -0700

    changed a few things in attempting to get it to work

--- build_tests.sh
+++ build_tests.sh
@@ -4,6 +4,7 @@ GTEST_DIR=lib/gtest-1.7.0
 PARSER_DIR="src/parser"
 PARSER_FILE="ConfigParser.cpp"
 PARSER_HEADER="config_parser.h"
+PARSER_PROCESSOR="ParserProcessor.cpp"
 PARSER_TEST_DIR="tests/parser"
 PARSER_TEST_FILE="ServerTest.cpp"
 BUILD_DIR="build"
@@ -12,7 +13,7 @@ case `uname` in
   Linux)
     g++ -std=c++0x -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -pthread -c ${GTEST_DIR}/src/gtest-all.cc -o ${BUILD_DIR}/gtest-all.o
     ar -rv ${BUILD_DIR}/libgtest.a ${BUILD_DIR}/gtest-all.o
-    g++ -std=c++0x -isystem ${GTEST_DIR}/include -pthread ${PARSER_DIR}/${PARSER_FILE} ${PARSER_TEST_DIR}/${PARSER_TEST_FILE} \
+    g++ -std=c++0x -isystem ${GTEST_DIR}/include -pthread ${PARSER_DIR}/${PARSER_FILE} ${PARSER_DIR}/${PARSER_PROCESSOR} ${PARSER_TEST_DIR}/${PARSER_TEST_FILE} \
     ${GTEST_DIR}/src/gtest_main.cc ${BUILD_DIR}/libgtest.a -o config_parser_test ;;
   Darwin)
     clang++ -std=c++11 -stdlib=libc++ -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -pthread -c ${GTEST_DIR}/src/gtest-all.cc -o ${BUILD_DIR}/gtest-all.o

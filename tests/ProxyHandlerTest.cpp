71e3bd9c6b5d72daa5dba595562ba609e6b8ae7f
Jonathan Joo <joo@caltech.edu>
2015-05-28 19:38:58 -0700

    edited the ProxyHandlerTest.cpp file to contain the test I wrote

--- tests/ProxyHandlerTest.cpp
+++ tests/ProxyHandlerTest.cpp
@@ -5,37 +5,59 @@
 
 static const char *test_config = "tests/parser/test_config";
 
-/* --- Function Prototypes --- */
-void print_map(strmap *string_map);
-
-/** 
- * Test a simple situation 
+/* 
+ * Test for ProxyHandler
  */
-TEST(ParserProcessorTest, GetPort1)
-{   
-    // Parse a mocked input
-    //const std::string config_text = "port 8080;";
-    //std::stringstream config_stream(config_text);
-    //NginxConfigParser parser;
-    //NginxConfig config;
-    //parser.Parse(&config_stream, &config);
 
-    // Make sure that the port number is as expected
-    //ParserProcessor parser_processor = ParserProcessor(config);
-    //unsigned short port = parser_processor.get_port();
-    EXPECT_EQ(0, 0);
-}
+class RequestHandlerTest : public ::testing::Test {
+    // Added 5/7/2015
+    // Assignment 5 unit tests
+
+    // Modified 5/24/2015 to work with new handlers.
+    protected:
+
+    bool HandleProxyResponse(const HTTPRequest &req, 
+            const std::string &host, const std::string &port, const std::string &expected) {
+        ProxyHandler a;
+        std::string output;
+        size_t check_http;
+        size_t check_domain;
+        size_t check_content;
+        size_t check_date;
+
+        NginxConfig config;
+        std::shared_ptr<NginxConfigStatement> statement (new NginxConfigStatement);
+        statement->tokens_.push_back("host");
+        statement->tokens_.push_back(host);
+        config.statements_.push_back(port);
+        a.Configure(config);
+
+        output = a.HandleRequest(req);
 
 
-/**
- * Debugging function: prints the given string map
- */
-void print_map(strmap *string_map)
-{
-    for ( unsigned i = 0; i < string_map->bucket_count(); ++i) {
-        std::cout << "bucket #" << i << " contains:";
-        for ( auto local_it = string_map->begin(i); local_it!= string_map->end(i); ++local_it )
-            std::cout << " " << local_it->first << ":" << local_it->second;
-        std::cout << std::endl;
-    }
+        check_http = output.find("HTTP/1.0 200 OK\r\n");
+        check_domain = output.find(expected);
+        check_content_type = output.find("Content-Type: text/html;\r\n");     
+        check_date = output.find("GMT");
+
+        if (check_http == std::string::npos ||
+            check_content_type == std::string::npos ||
+            check_date == std::string::npos ||
+            check_domain == std::string::npos) {
+            return 0;
+        }
+        return 1;     
+    }  
+
+};
+
+// Request Handler Tests:
+TEST_F(RequestHandlerTest, HandleProxyResponse) {
+    // Tests the google.com proxy from port 80.
+    HTTPRequest test_static1;
+    test_static1.method = "GET";
+    test_static1.path = "/reverse"; 
+    EXPECT_TRUE(HandleProxyResponse(test_static1, "www.google.com", "80", "domain=.google.com"));
+
+
 }
